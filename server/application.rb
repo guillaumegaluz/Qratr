f4f4fb0db6057b59dabe14ba9591a7366c151214
Peter Graham <peter@1000memories.com>
2012-08-31 09:07:41 -0700

    Extract helpers to a separate file

--- server/application.rb
+++ server/application.rb
@@ -25,22 +25,3 @@ class App < Sinatra::Base
   set :public_folder, Proc.new { File.join(root, "client/public") }
   set :views, Proc.new { File.join(root, "client/views") }
 end
-
-App.helpers do
-  def haml_partial(page, options={})
-    haml page, options.merge!(:layout => false)
-  end
-
-  def erb_partial(page, options={})
-    erb page, options.merge!(:layout => false)
-  end
-
-  def json(body_hash, status=200, headers={})
-    body = if params[:readable]
-      JSON.pretty_generate(body_hash)
-    else
-      body_hash.to_json
-    end
-    halt status, headers.merge({'Content-Type' => 'application/json'}), body
-  end
-end
---
+++ server/helpers.rb
@@ -0,0 +1,18 @@
+App.helpers do
+  def haml_partial(page, options={})
+    haml page, options.merge!(:layout => false)
+  end
+
+  def erb_partial(page, options={})
+    erb page, options.merge!(:layout => false)
+  end
+
+  def json(body_hash, status=200, headers={})
+    body = if params[:readable]
+      JSON.pretty_generate(body_hash)
+    else
+      body_hash.to_json
+    end
+    halt status, headers.merge({'Content-Type' => 'application/json'}), body
+  end
+end

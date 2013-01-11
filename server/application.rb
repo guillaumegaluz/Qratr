7f6802f68d0251b6ac6a06c85596947d6c83e776
Peter Graham <peterghm@gmail.com>
2013-01-11 01:23:57 -0800

    Don't raise errors or show exceptions on production & test

--- server/application.rb
+++ server/application.rb
@@ -30,6 +30,11 @@ end
 
 set :sprockets, SprocketsEnvironmentBuilder.build(ENV['RACK_ENV'])
 
+configure :production, :test do
+  set :raise_errors, false
+  set :show_exceptions, false
++end
+
 configure :production do
   require 'newrelic_rpm'
   # Ensure the agent is started using Unicorn

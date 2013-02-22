6f9a5d0fc94826f4d9280362184459cf406e8121
Peter Graham <peterghm@gmail.com>
2013-02-21 20:38:07 -0800

    Add example JSON endpoint

--- server/controllers.rb
+++ server/controllers.rb
@@ -2,4 +2,8 @@ class App < Sinatra::Base
   get '/' do
     haml :index
   end
+
+  get '/example-json-endpoint' do
+    json({:some => "json"})
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,4 +7,16 @@ describe App do
       last_response.should be_ok
     end
   end
+
+  describe "GET /example-json-endpoint" do
+    it "returns OK" do
+      get "/example-json-endpoint"
+      last_response.should be_ok
+    end
+
+    it "returns expected JSON" do
+      get "/example-json-endpoint"
+      JSON.parse(last_response.body).should == {"some" => "json"}
+    end
+  end
 end

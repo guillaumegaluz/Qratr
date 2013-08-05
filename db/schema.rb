d4aaa76054f0532074c2afe39f49b85b73694b8c
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-08-04 17:19:40 -0700

    Ad playlits_id to tracks tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130804222221) do
+ActiveRecord::Schema.define(:version => 20130804224239) do
 
   create_table "playlists", :force => true do |t|
     t.string   "name"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20130804222221) do
     t.string   "artwork_url"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.integer  "playlist_id"
   end
 
 end

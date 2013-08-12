1d910a57fe819872acdc01d06ef6fa4ab8b0a34b
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-08-11 18:29:52 -0700

    Remove test for exampel endpoint

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,23 +7,4 @@ describe App do
       last_response.should be_ok
     end
   end
-
-  describe "GET /example-json-endpoint" do
-    it "returns OK" do
-      get "/example-json-endpoint"
-      last_response.should be_ok
-    end
-
-    it "returns expected JSON" do
-      get "/example-json-endpoint"
-      JSON.parse(last_response.body).should == {"some" => "json"}
-    end
-
-    context "with callback param" do
-      it "wraps response JSON in JSONP callback" do
-        get "/example-json-endpoint?callback=what"
-        last_response.body.should match(/^what\(.+\)$/)
-      end
-    end
-  end
 end

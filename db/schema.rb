60f8344032e99fa228b153810c101e8053558d3a
Guillaume Galuz <guillaume.galuz@gmail.com>
2013-08-20 20:58:11 -0700

    Add soundcloud_id and stream_url to tracks table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130804224239) do
+ActiveRecord::Schema.define(:version => 20130821031555) do
 
   create_table "playlists", :force => true do |t|
     t.string   "name"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(:version => 20130804224239) do
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
     t.integer  "playlist_id"
+    t.integer  "soundcloud_id"
+    t.string   "stream_url"
   end
 
 end
